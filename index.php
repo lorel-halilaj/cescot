7197f13443d3acf596df0ca4bcaf92b13b3f9ec0
Lorel <halilaj.lorel@gmail.com>
2025-11-18 17:23:51 +0100

    add initial PHP script and launch configuration for debugging

---
+++ index.php
@@ -0,0 +1,15 @@
+<?php
+
+    echo "Hello World";
+
+    echo "<br>";
+
+    $variable = 42;
+    echo "The answer is $variable.";
+
+    if ($variable > 40) {
+        echo "The variable is grater than 40.";
+    } else {
+        echo "The variable is not greater than 40.";
+    };
+?>
